f833c7e8e344a0e3e349014dd651e70c7ca77f90
DamilolaAjayi <ajayidamey@gmail.com>
2019-09-25 21:03:47 +0100

    signup page

--- frontend/Signup page/signup.js
+++ frontend/Signup page/signup.js
@@ -1,14 +1,12 @@
 // listing all text elements
 var email = document.getElementById['email'];
 var password = document.getElementById['password'];
+var confirmPassword = document.getElementById('confirmPassword');
+var errorMessage = document.getElementById('errorMessage');
 
 // listing all error display elements 
 var email_error = document.getElementById('email_error');
 var password_error = document.getElementById('password_error');
-
-// listing all event listeners 
-email.addEventListener('blur', emailVerify, true);
-password.addEventListener('blur', passwordVerify, true);
 
 // validate function
 function Validate() {
@@ -21,7 +19,7 @@ function Validate() {
         return false;
     }
 
-    if (password.value == "" && password.value.length < 6) {
+    if (password.value == "") {
         password.style.border = "1px solid red";
         document.getElementById('password').style.color = "red";
         password_error.textContent = "Password is required";
@@ -29,20 +27,37 @@ function Validate() {
         return false;
     }
 }
-// event handler functions 
-function emailVerify() {
+
+email.addEventListener('keyup', function() {
     if (email.value != "") {
         email.style.border = "1px solid #5e6e66";
         document.getElementById('email').style.color = "#5e6e66";
         email_error.innerHTML = "";
+
         return true;
     }
-}
-function passwordVerify() {
+});
+
+password.addEventListener('keyup',function() {
     if (password.value != "") {
         password.style.border = "1px solid #5e6e66";
         document.getElementById('password').style.color = "#5e6e66";
         password_error.innerHTML = "";
+
         return true;
     }
-}
+});
+
+confirmPassword.addEventListener('keyup' , function() {
+    if (password.value !== confirmPassword.value) {
+  
+      errorMessage.style.display = "block";
+  
+    } else if (password.value === confirmPassword.value){
+  
+      errorMessage.style.display = "none";
+    }
+})
+
+    
+
