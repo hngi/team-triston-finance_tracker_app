8e253cf78975b7dd08c169b7ad59ad0f4891720b
Nefe Emadamerho <nefejames@yahoo.com>
2019-10-04 04:21:16 -0700

    Faq page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,14 @@
 import React, { useState } from 'react';
-
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
 import { UserContextProvider } from './UserContext';
 import { Footer } from "./components";
-
 import { PrivateRoute } from './components/PrivateRoute';
 import Expense from './pages/Expense';
 import ForgotPassword  from './pages/ForgotPassword';
 import Login from './pages/Login';
 import Report from './pages/Report';
 import Signup from './pages/Signup';
-
+import faq from './pages/faq';
 
 function App() {
 
@@ -26,6 +23,7 @@ function App() {
             <Switch>
               <Route exact path="/" component={ Login } />
               <Route path="/forgot-password" component={ ForgotPassword } />
+              <Route path="/faq-page" component={ faq } />
               <Route path="/get-started" component={ Signup } />
               <PrivateRoute isAuthenticated = { user.isLoggedIn } path="/add-expense" component={ Expense } />
               <PrivateRoute isAuthenticated = { user.isLoggedIn } path="/report" component={ Report } />
